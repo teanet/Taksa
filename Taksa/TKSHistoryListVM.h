6f9784c5a6557813269ee60a724d8943ae076080
Смирнов Вадим Павлович <va.smirnov@2Gis.ru>
2016-07-14 00:52:15 +0600

    [TKS-17] Hide keyboard with scroll.

--- Taksa/TKSHistoryListVM.h
+++ Taksa/TKSHistoryListVM.h
@@ -3,6 +3,7 @@
 @interface TKSHistoryListVM : NSObject
 
 @property (nonatomic, strong, readonly) RACSignal *didSelectSuggestSignal;
+@property (nonatomic, strong, readonly) RACSignal *didScrollSignal;
 @property (nonatomic, strong) NSArray<TKSSuggest *> *suggests;
 
 - (void)registerTableView:(UITableView *)tableView;
